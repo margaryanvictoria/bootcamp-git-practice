71c04e71f54e30323cbfb6e9876a46b9dbd6bf23
margaryanvictoria <margaryanvictoria@gmail.com>
2020-07-27 12:24:41 -0700

    Set up and configured a basic Tailwind project.

---
+++ tailwind-start/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <label className="text-xs">Hello, World!</label>
+      <label className="text-sm">Hello, World!</label>
+      <label className="text-md">Hello, World!</label>
+      <label className="text-lg">Hello, World!</label>
+      <label className="text-xl">Hello, World!</label>
+      <label className="text-2xl">Hello, World!</label>
+      <label className="text-3xl">Hello, World!</label>
+    </div>
+  );
+}
